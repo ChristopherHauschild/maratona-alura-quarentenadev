e48f8e576ffdc4964b4feed4b71c63ffc0b0f373
Christopher Hauschild Schott <chrishaus01@gmail.com>
2020-04-05 14:09:41 -0300

    Create exercicios.js

---
+++ aula_3/exercicios.js
@@ -0,0 +1,10 @@
+QUEBRANDO LINHAS
+
+document.write('Oi, <br> como <br> você <br> está <br> pulando <br> linha <br> ?')
+
+LITROS COMBUSTÍVEL
+
+km_l = 12
+horas = 10
+vel = 81
+document.write('Foram necessários '+ (81*10) / 12 + ' litros de combustível')
