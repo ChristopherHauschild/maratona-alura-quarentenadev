fa191fb5a6e9c773157ee9f9ee9632dbb4b66268
Christopher Hauschild Schott <chrishaus01@gmail.com>
2020-04-06 19:54:00 -0300

    Create ficha.js

---
+++ aula_6/ficha/ficha.js
@@ -0,0 +1,13 @@
+//nome = prompt("Nome completo")
+tagh1.innerHTML = "Nome: " + nome
+
+nascimento = "05/02/00"
+temcondicao = confirm("Tem condição pré-existente?")
+
+if(temcondicao){estadodesaude = "Sim"}
+if(temcondicao == false){estadodesaude = "Nao"}
+
+lista.innerHTML =
+  '<li> Data de Nascimento: ${nascimento} </li>
+   <li> Idade: 23 anos </li> <li> Tem condição pré-existente: ${estado de saude} </li>
+   <li> Precisa de acompanhamento médico </li>'
