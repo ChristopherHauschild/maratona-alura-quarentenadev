093af9089d665e58a155cb0910374e892a8ded43
Christopher Hauschild Schott <chrishaus01@gmail.com>
2020-04-09 09:31:16 -0300

    Update sorteio.js

--- aula_7/sorteio.js
+++ aula_7/sorteio.js
@@ -3,7 +3,10 @@ function ExecutarSorteio()
   aleatorio = Math.round(Math.random()*10)
   chute = prompt('Seu numero da sorte (1-10)')
   
-  if( chute < 0 || chute > 10 ){chute = prompt('Informe um numero entre 1 e 10')}
+  if( chute < 0 || chute > 10 ){
+    alert('Você informou um número inválido. Informe um número entre 1 e 10.')
+    chute = prompt('Seu numero da sorte (1-10)')
+  }
   
   numerocerto = chute == aleatorio
   
