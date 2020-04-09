a06262fbd7702b376941eaf88c86e4f933c3f903
Christopher Hauschild Schott <chrishaus01@gmail.com>
2020-04-09 10:04:50 -0300

    Create desafios.js

---
+++ aula_8/desafios.js
@@ -0,0 +1,26 @@
+DESAFIO DO MARIO
+
+primeiraOlimpiada = 1896;
+anoAtual = prompt('Informe o ano atual');
+
+document.write('Até este ano tivemos ' + (anoAtual - primeiraOlimpiada) / 4 + ' Olimpiadas');
+
+DESAFIO DO ART
+
+n = 0;
+
+while(n < 500){
+	n += 1;
+	document.write(n + " - ");
+}
+
+DESAFIO EXTRA
+
+n = 0;
+
+while(n < 100){
+	n += 1;
+	if(n % 2 ==0){
+		document.write(n + " - ");
+	}
+}	
